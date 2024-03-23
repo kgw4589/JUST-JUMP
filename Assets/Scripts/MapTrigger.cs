894b45edac86336f69a9f8eb6a541e6a64dc1aee
Yoimiya <kgw4589>
2024-03-23 13:23:18 +0900

    MapTrigger.cs, Player, Prefab 변경
    변경사항
    
    MapTrigger.cs
    - 기본 틀 코드 작성
    -- 사용법
    -- PlayerObj에 플레이어 오브젝트 추가
    -- 이번트를 처음 닿았을 때 한 번만 실행하려면 DestroyTriggerObject에 체크
    -- TriggerEnter에 발생할 이벤트를 EnterEvent에 추가
    -- TriggerExit에 발생할 이벤트를 ExitEvent에 추가
    -- (웬만하면 MapTrigger.cs에 이벤트 코드 작성, 현재 예시 추가해놓은 상태)
    
    Player
    - Player오브젝트에 box collider2D, Rigidbody2D, Player.cs 추가
    
    Prefab
    - MapTrigger.cs가 추가된 MapTrigger 오브젝트를 프리펩화하여 추가

--- Assets/Scripts/MapTrigger.cs
+++ Assets/Scripts/MapTrigger.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MapTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject playerObj;
+    private Player _player;
+    
+    [SerializeField] private bool destroyTriggerObject;
+    [SerializeField] private UnityEvent onTriggerEnterEvent;
+    [SerializeField] private UnityEvent onTriggerExitEvent;
+
     void Start()
     {
+        _player = playerObj.GetComponent<Player>();
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject != playerObj) return;
         
+        onTriggerEnterEvent.Invoke();
+        
+        if (destroyTriggerObject) Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerExit2D(Collider2D other)
     {
+        if (other.gameObject != playerObj) return;
         
+        onTriggerExitEvent.Invoke();
+    }
+
+    public void ChangeSpeed(float speed)
+    {
+        // _player.GetComponent<Player>().ChangeJumpPower(speed);
     }
 }
