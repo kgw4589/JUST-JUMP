0a8ca09932c4bd72a581a04d260c7b61469f4d5c
Yoimiya <kgw4589@gmail.com>
2024-03-25 20:21:56 +0900

    MapManager.cs 작성 및 Map 기본 틀 오브젝트 변경
    추가
    - MapManager 오브젝트 추가
    
    변경
    -MapManager.cs (임시)
    -- 게임 시작 시 MapManager 오브젝트 내 Maps 리스트 중 랜덤하게 골라 맵 생성(임시로 3개)
    -- 크기 및 위치는 알아서 맞춤
    -- 처음 생성되는 맵의 포지션 조절 변수로 위치 변경 가능(Inspector창에서도 가능)
    
    맵 생성방법(앞으로 맵을 만들 때)
    1. Prefab 폴더의 MapSize 프리펩을 끌고 온다.
    2. 끌고온 MapSize 프리펩을 해제.
    3. 오브젝트 내 MainBechi(갈색 테두리)의 크기에 맞춰 알아서 맵 생성.
    4. 완성된 맵을 Maps 폴더에 드래그 앤 드롭으로 프리펩화.
    5. 해당 프리펩을 MapManager 오브젝트의 Maps 리스트에 추가.

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject[] maps;
+    
+    [SerializeField] private Vector2 startPos;
+    
+    private GameObject _lastMap;
+    private Vector3 _interval;
+    
     void Start()
     {
+        _interval = new Vector2(0, maps[0].transform.localScale.y);
         
+        _lastMap = Instantiate(maps[Random.Range(0, maps.Length)]);
+        _lastMap.transform.position = startPos;
+        InstantiateRandomMap();
+        InstantiateRandomMap();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InstantiateRandomMap()
     {
-        
+        GameObject map = Instantiate(maps[Random.Range(0, maps.Length)]);
+        map.transform.position = _lastMap.transform.position += _interval;
     }
 }
