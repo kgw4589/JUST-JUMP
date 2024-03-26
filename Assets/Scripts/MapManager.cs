41fc4992756840aeb6b20b74380a5891655c19b0
Yoimiya <kgw4589@gmail.com>
2024-03-26 20:33:03 +0900

    MapManager.cs, Hierarchy창, MapSize프리펩 업데이트
    내용
    
    업데이트
    - MapManager.cs
    -- 플레이어의 위치에 따라 맵 생성
    -- GameOverZone 위치에 따라 맵 삭제
    --- (GAmeOverZone이 없어 임시로 GmaeManager의 위치로 지정)
    - Hierachy 오브젝트 업데이트
    - MapSize 프리펩 collider 추가

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -7,18 +7,36 @@ public class MapManager : MonoBehaviour
     [SerializeField] private GameObject[] maps;
     
     [SerializeField] private Vector2 startPos;
-    
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject gameOverZone;
+
     private GameObject _lastMap;
     private Vector3 _interval;
-    
+    private float _mapSizeY;
+
+    public Queue<GameObject> _mapPosQueue = new Queue<GameObject>();
+
     void Start()
     {
-        _interval = new Vector2(0, maps[0].transform.localScale.y);
+        _mapSizeY = maps[0].transform.localScale.y;
+        _interval = new Vector2(0, _mapSizeY);
         
         _lastMap = Instantiate(maps[Random.Range(0, maps.Length)]);
         _lastMap.transform.position = startPos;
+        _mapPosQueue.Enqueue(_lastMap);
+        
         InstantiateRandomMap();
         InstantiateRandomMap();
+    }
+
+    void Update()
+    {
+        float dis = Vector2.Distance(player.transform.position, _lastMap.transform.position);
+        if (dis < _mapSizeY * 3) InstantiateRandomMap();
+        Vector3 _mapQueFirst = _mapPosQueue.Peek().transform.position;
+        if (gameOverZone.transform.position.y - _mapQueFirst.y > _mapSizeY)
+            Destroy(_mapPosQueue.Dequeue());
     }
 
     void InstantiateRandomMap()
@@ -26,5 +44,6 @@ public class MapManager : MonoBehaviour
         GameObject map = Instantiate(maps[Random.Range(0, maps.Length)]);
         map.transform.position = _lastMap.transform.position + _interval;
         _lastMap = map;
+        _mapPosQueue.Enqueue(_lastMap);
     }
 }
