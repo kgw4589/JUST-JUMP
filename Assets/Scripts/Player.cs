1d3930e9ec22c73a20fd5ad3d2b72d37153f940a
duck3866 <130462482+duck3866@users.noreply.github.com>
2024-03-27 17:32:23 +0900

    Feat :: Player Jump
    스와이프 방식 플레이어 점프함

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rd;
+    
+    private Vector2 strPos;
+    private Vector2 endPos;
+    private Vector2 direction;
+    
+    public float JumpPower = 5f;
+    [SerializeField] private float maxPower = 5f;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rd = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            strPos = Input.mousePosition;
+        }
+    
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPos = Input.mousePosition;
+            direction = strPos - endPos;
+            JumpPower = direction.magnitude/20; // 스와이프 길이 계산
+            if (JumpPower > maxPower)
+            {
+                JumpPower = maxPower;
+            }
+            direction.Normalize();
+            Debug.Log(JumpPower);
+            Jump(direction);
+        }
+    }
+
+    void Jump(Vector2 dir)
+    {
+        rd.AddForce(new Vector2(dir.x,dir.y)* JumpPower,ForceMode2D.Impulse);
     }
 }
