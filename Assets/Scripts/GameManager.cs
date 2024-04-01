c03bdc030045b681474f3ad89fa4a8fcbc56a901
duck3866 <130462482+duck3866@users.noreply.github.com>
2024-04-01 20:10:34 +0900

    fix :: Start Player Move
    시작화면때 플레이어가 드래그가 되는 것과 그것이 중첩되어 시작하자말자 날아가는 버그 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _player;
+    public GameObject _player;
     
     [SerializeField]
     private int _highScore;
@@ -13,11 +13,18 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private float _playerPosY;
     public bool isPlay;
-    
+    private Player _playerScript;
+
+    private void Start()
+    {
+        _playerScript = _player.GetComponent<Player>();
+    }
+
     public void StartGame()
     {
         Time.timeScale = 1; // game start
         isPlay = true;
+        _playerScript.Invoke("WaitStart",0.1f);
     }
 
     public void PauseGame()
