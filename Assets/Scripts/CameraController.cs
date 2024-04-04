e0b4dbabe08f9e0a222052087769cdf07fd47e24
Yoimiya <kgw4589@gmail.com>
2024-04-04 20:53:01 +0900

    feat :: 해상도에 맞춰 카메라 사이즈를 조절해 가로 범위를 동일케함

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,39 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
     public Vector3 offset;
-    
-    void Awake()
+    private float _targetWidth = 6f; // 가로 방향으로 보여주고 싶은 유닛의 수
+
+    void Start()
     {
-        // var camera = GetComponent<Camera>();
-        // var r = camera.rect;
-        // var scaleHeight = ((float)Screen.width / Screen.height) / (9f / 16f);
-        // var scaleWidth = 1f / scaleHeight;
-        //
-        // if (scaleHeight < 1f)
-        // {
-        //     r.height = scaleHeight;
-        //     r.y = (1f - scaleHeight) / 2f;
-        // }
-        // else
-        // {
-        //     r.width = scaleWidth;
-        //     r.x = (1f - scaleWidth) / 2f;
-        // }
-        //
-        // camera.rect = r;
+        var camera = GetComponent<Camera>();
+
+        // 가로세로 비율에 따라 orthographicSize 조정
+        float windowAspect = (float)Screen.width / (float)Screen.height;
+        camera.orthographicSize = (_targetWidth / windowAspect) / 2f;
     }
 
-    // void OnPreCull() => GL.Clear(true, true, Color.black);
-    
     void Update()
     {
-        var dirPos = new Vector3(0, player.transform.position.y, 0) + offset;
+        var dirPos = player.transform.position + offset;
         
-        transform.position = Vector3.Lerp(transform.position, dirPos, 1f);
+        // 카메라를 플레이어를 따라가게 하되, z축은 변경하지 않음
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, dirPos.y, transform.position.z), Time.deltaTime);
     }
 }
