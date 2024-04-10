2f1ec09fef11dbe341c7b9b261ffb5fea00dc34f
YoimiyaKazusa <kgw4589@gmail.com>
2024-04-10 13:48:46 +0900

    refactor :: CameraController.cs
    Start 안에 있던 카메라 사이즈 설정을 따로 public 함수로 빼내어 언제라도 호출이 가능케 함

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,22 +4,29 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
     public Vector3 offset;
+    
     private float _targetWidth = 6f; // 가로 방향으로 보여주고 싶은 유닛의 수
+
+    private Camera _camera;
 
     void Start()
     {
-        var camera = GetComponent<Camera>();
+        _camera = GetComponent<Camera>();
+        InitCameraSize();
+    }
 
+    public void InitCameraSize()
+    {
         // 가로세로 비율에 따라 orthographicSize 조정
         float windowAspect = (float)Screen.width / (float)Screen.height;
-        camera.orthographicSize = (_targetWidth / windowAspect) / 2f;
+        _camera.orthographicSize = (_targetWidth / windowAspect) / 2f;
     }
 
     void Update()
     {
-        var dirPos = player.transform.position + offset;
+        var dirPosition = player.transform.position + offset;
+        Vector3 moveDir = new Vector3(transform.position.x, dirPosition.y, transform.position.z);
         
-        // 카메라를 플레이어를 따라가게 하되, z축은 변경하지 않음
-        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, dirPos.y, transform.position.z), Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, moveDir, Time.deltaTime);
     }
 }
