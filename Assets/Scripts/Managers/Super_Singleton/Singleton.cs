45dc80f639d8d3939e1c2128cbea2903c2cc84a7
Yoimiya <kgw4589@gmail.com>
2024-04-13 14:13:40 +0900

    feat :: 싱글톤을 구현할 때 상속만 받으면 되도록 싱글톤 부모 클래스 구현
    싱글톤으로 만들고 싶을 때 MonoBehaviour 대신 Singleton<클래스명> 상속 받으면 끝

---
+++ Assets/Scripts/Managers/Super_Singleton/Singleton.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+    
+                if (_instance == null)
+                {
+                    GameObject gameObject = new GameObject(typeof(T).Name, typeof(T));
+                    _instance = gameObject.GetComponent<T>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (transform.parent != null && transform.root != null)
+        {
+            DontDestroyOnLoad(this.transform.root.gameObject);
+        }
+        else
+        {
+             DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
