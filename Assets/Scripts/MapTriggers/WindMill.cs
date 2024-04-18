ecdc439c54ee5d7589f8b641522c107e3e721146
harp515 <harp5151017@gmail.com>
2024-04-18 18:17:17 +0900

    feat :: WindMill 추가
    turnSpeed 조절 시 회전 속도 조절 가능, 날개 개수는 알아서 조정

---
+++ Assets/Scripts/MapTriggers/WindMill.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindMil : MonoBehaviour
+{
+    [SerializeField] private float turnSpeed = 0.5f;
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0,0,turnSpeed));
+    }
+}
