53ee934c6512e2ed2b8cfa45c4128bf9f8d359a8
kong2ys <131360841+kong2ys@users.noreply.github.com>
2024-06-27 20:23:25 +0900

    feat :: GameOverZone Interface add

--- Assets/Scripts/GameOverZone.cs
+++ Assets/Scripts/GameOverZone.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameOverZone : MonoBehaviour
+public class GameOverZone : MonoBehaviour, IObjectInit
 {
     [SerializeField] private float _waitTime = 3f; // second
     [SerializeField] private float _moveDistance = 3f; // Y Axis
@@ -14,6 +14,17 @@ public class GameOverZone : MonoBehaviour
     private int _contactCnt = 0;
 
     private Player _player;
+
+    private void Awake()
+    {
+        GameManager.Instance.SetInitDelegate(this);
+    }
+
+    public void InitObject()
+    {
+        gameObject.SetActive(false);
+    }
+    
     void Start()
     {
         _player = GameObject.FindWithTag("Player").GetComponent<Player>();
@@ -75,4 +86,5 @@ public class GameOverZone : MonoBehaviour
             _contactCnt = 0;
         }
     }
+    
 }
