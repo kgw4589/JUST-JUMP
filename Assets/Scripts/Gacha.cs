5b025d9585df5930be68185be070e33e821d0650
Yoimiya <kgw4589@gmail.com>
2024-08-13 19:49:19 +0900

    Feat / Gacha :: 버튼을 통해 뽑기 기능

--- Assets/Scripts/Gacha.cs
+++ Assets/Scripts/Gacha.cs
@@ -1,44 +1,62 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class Gacha : MonoBehaviour
 {
-    private const string SHEET_URL =
-        "https://docs.google.com/spreadsheets/d/1fXMD0-E3BzRYGxw1NP9vNgQME82UK3_nQsQUexYfYzo/export?format=tsv&range=A2:C7";
-
-    private string[] _sheetDataRow;
-
     private enum Probability
     {
-        Normal = 700, Epic = 250, Legend = 50
+        Normal = 700,
+        Epic = 250,
+        Legend = 500
     }
     private int _totalProbability;
     private int _currentPivot;
+    
+    private Dictionary<Probability, string> _sheetURL = new Dictionary<Probability, string>()
+    {
+        { Probability.Normal , "https://docs.google.com/spreadsheets/d/1fXMD0-E3BzRYGxw1NP9vNgQME82UK3_nQsQUexYfYzo/export?format=tsv&range=A2:C4"},
+        { Probability.Epic , "https://docs.google.com/spreadsheets/d/1RzikpWotuwisLCb3Tk2-IKyTTldfbQMozatybseHVzI/export?format=tsv&range=A2:C3"},
+        { Probability.Legend , "https://docs.google.com/spreadsheets/d/1z_Ep63oibFqS7L808ab9Bu1lOCxEMSxXrEagNNGU2Oo/export?format=tsv&range=A2:C2"}
+    };
 
-    private Dictionary<string, int> _ratingDictionary = new Dictionary<string, int>();
+    private Dictionary<Probability, string> _sheetData = new Dictionary<Probability, string>();
 
-    private IEnumerator Start()
+    private string[] _sheetDataRow;
+
+    [SerializeField] private TextMeshProUGUI tempRatingText;
+    [SerializeField] private TextMeshProUGUI tempGachaName;
+    [SerializeField] private Image tempGachaImage;
+
+    private void Start()
     {
         _totalProbability = 0;
         _currentPivot = 0;
-        foreach (int probability in Enum.GetValues(typeof(Probability)))
-        {
-            _totalProbability += probability;
-        }
 
-        // UnityWebRequest 인스턴스 리소스 해제를 위해 Using 사용함.
-        using (UnityWebRequest www = UnityWebRequest.Get(SHEET_URL))
-        {
-            yield return www.SendWebRequest();
+        StartCoroutine(SetSheetData());
+    }
 
-            if (www.isDone)
+    IEnumerator SetSheetData()
+    {
+        foreach (Probability probability in Enum.GetValues(typeof(Probability)))
+        {
+            _totalProbability += (int)probability;
+            
+            // UnityWebRequest 인스턴스 리소스 해제를 위해 Using 사용함.
+            using (UnityWebRequest www = UnityWebRequest.Get(_sheetURL[probability]))
             {
-                SetData(www.downloadHandler.text);
+                yield return www.SendWebRequest();
+
+                if (www.isDone)
+                {
+                    _sheetData.Add(probability, www.downloadHandler.text);
+                }
             }
         }
     }
@@ -47,31 +65,30 @@ public class Gacha : MonoBehaviour
     {
         _sheetDataRow = sheetData.Split("\n");
 
-        foreach (var data in _sheetDataRow)
-        {
-            if (!_ratingDictionary.TryAdd(data[0].ToString(), 0))
-            {
-                ++_ratingDictionary[data[0].ToString()];
-            }
-        }
+        int value = Random.Range(0, _sheetDataRow.Length);
 
-        PlayGacha();
+        var a = _sheetDataRow[value].Split("\t");
+
+        tempRatingText.text = a[0];
+        tempGachaName.text = a[1];
     }
 
     public void PlayGacha()
     {
         int randomValue = Random.Range(1, _totalProbability + 1);
 
-        foreach (int probability in Enum.GetValues(typeof(Probability)))
+        foreach (Probability probability in Enum.GetValues(typeof(Probability)))
         {
-            Debug.Log("확률 : " + probability);
-            _currentPivot += probability;
+            _currentPivot += (int)probability;
 
             if (randomValue <= _currentPivot)
             {
-                Debug.Log((Probability)probability);
+                Debug.Log(probability);
+                SetData(_sheetData[probability]);
                 break;
             }
         }
+
+        _currentPivot = 0;
     }
 }
