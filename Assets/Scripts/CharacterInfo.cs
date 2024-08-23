2a88f873177bb0d7c9f3be5d305d1649cd983b66
YoimiyaKazusa <kgw4589@gmail.com>
2024-08-23 19:31:57 +0900

    Reapply "Refactor / Gacha :: 캐릭터 정보 방식 변경"
    This reverts commit 711fffb0027febf6bdc9aabb0ded00f682329e55.

---
+++ Assets/Scripts/CharacterInfo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInfo
+{
+    public int characterId;
+        
+    public Gacha.Probability characterRating;
+        
+    public string characterName;
+    public Sprite characterImage;
+
+    public int characterIndex;
+}
