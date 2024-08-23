711fffb0027febf6bdc9aabb0ded00f682329e55
YoimiyaKazusa <kgw4589@gmail.com>
2024-08-23 19:31:42 +0900

    Revert "Refactor / Gacha :: 캐릭터 정보 방식 변경"
    This reverts commit 764fe6f25f252c762d4f831f9be89a5925e9c84e.

--- Assets/Scripts/Gacha.cs
+++ Assets/Scripts/Gacha.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class Gacha : MonoBehaviour
 {
-    public enum Probability
+    private enum Probability
     {
         Normal = 700,
         Epic = 250,
@@ -20,11 +23,8 @@ public class Gacha : MonoBehaviour
     [SerializeField] private TextMeshProUGUI tempGachaName;
     [SerializeField] private Image tempGachaImage;
 
-    private CharacterInfo _characterInfo;
-
-    private List<CharacterInfo> _normalCharacters = new List<CharacterInfo>();
-    private List<CharacterInfo> _epicCharacters = new List<CharacterInfo>();
-    private List<CharacterInfo> _legendCharacters = new List<CharacterInfo>();
+    private CharacterScriptable _characterScriptable;
+    private List<CharacterScriptable.CharacterInfo> _haveNotCharacters = new List<CharacterScriptable.CharacterInfo>();
 
     private void Awake()
     {
@@ -34,6 +34,21 @@ public class Gacha : MonoBehaviour
         {
             _totalProbability += (int)probability;
         }
+        
+        _characterScriptable = Resources.Load<CharacterScriptable>("AllCharacterList");
+        
+        // foreach (var characterInfo in _characterScriptable.normalCharacters)
+        // {
+        //     _haveNotCharacters.Add(characterInfo);
+        // }
+        // foreach (var characterInfo in _characterScriptable.epicCharacters)
+        // {
+        //     _haveNotCharacters.Add(characterInfo);
+        // }
+        // foreach (var characterInfo in _characterScriptable.legendCharacters)
+        // {
+        //     _haveNotCharacters.Add(characterInfo);
+        // }
     }
 
     public void PlayGacha()
@@ -57,12 +72,36 @@ public class Gacha : MonoBehaviour
 
     private void SelectedCharacter(Probability probability)
     {
+        CharacterScriptable.CharacterInfo selectedCharacter = default;
+        List<CharacterScriptable.CharacterInfo> selectedInfos;
         
+        switch (probability)
+        {
+            // case Probability.Normal :
+            //     selectedInfos = _characterScriptable.normalCharacters;
+            //     selectedCharacter = selectedInfos[Random.Range(0, selectedInfos.Count)];
+            //     selectedCharacter.ratingText = _characterScriptable.normalText;
+            //     break;
+            // case Probability.Epic :
+            //     selectedInfos = _characterScriptable.epicCharacters;
+            //     selectedCharacter = selectedInfos[Random.Range(0, selectedInfos.Count)];
+            //     selectedCharacter.ratingText = _characterScriptable.epicText;
+            //     break;
+            // case Probability.Legend :
+            //     selectedInfos = _characterScriptable.legendCharacters;
+            //     selectedCharacter = selectedInfos[Random.Range(0, selectedInfos.Count)];
+            //     selectedCharacter.ratingText = _characterScriptable.legendText;
+            //     break;
+        }
+
+        _haveNotCharacters.Remove(selectedCharacter);
+        SetData(selectedCharacter);
     }
 
-    private void SetData()
+    private void SetData(CharacterScriptable.CharacterInfo character)
     {
-        // tempGachaImage.sprite = character.characterImage;
-        // tempGachaName.text = character.characterName;
+        tempRatingText.text = character.ratingText;
+        tempGachaImage.sprite = character.characterImage;
+        tempGachaName.text = character.characterName;
     }
 }
